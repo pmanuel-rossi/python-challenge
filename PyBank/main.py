73162098d7e2de65af64a6a687131597b58f24b9
pmanuel-rossi <pedromanuel.rossi@gmail.com>
2019-06-18 13:21:11 -0500

    Bank commit
    3 first questions answered

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,44 @@
+# Bank homework
 
+#packages
+
+import pandas as pd
+import numpy as np
+
+bankPath = r'/Users/pedrorossi/Documents/GitHub/python-challenge/PyBank/resources'
+
+bankFile = '/Week 3 - Python-Homework-Instructions-PyBank-Resources-budget_data.csv'
+
+bankDB = pd.read_csv(bankPath + bankFile)
+
+#Number of months 
+
+months = len(bankDB)
+
+#total Profit / loss
+
+totalProfit = bankDB['Profit/Losses'].sum()
+
+print(totalProfit)
+
+#average change
+
+profitLossesList = list(bankDB['Profit/Losses'])
+dateProfitLossesList = bankDB['Date']
+
+changeList = []
+counter = 0
+
+numDB = np.array(dateProfitLossesList)
+date = ""
+
+for i in range(len(bankDB)-1):
+    curr = profitLossesList[i]
+    currDown = profitLossesList[i+1]
+    changeList.append(currDown-curr)
+    counter = sum(bankDB['Profit/Losses'])
+    minChange = min(changeList)
+    maxChange = max(changeList)  
+    avgChance = round(np.mean(changeList),2)
+    if i == minChange - maxChange:
+        date = numDB[i]
